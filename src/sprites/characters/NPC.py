cb66eb2533c2ba68f0236517294b048fbeaa3d01
Rafael Alcalde Azpiazu <nekerafa@gmail.com>
2018-03-27 18:35:26 +0200

    Añadido ventana mágica

--- src/sprites/characters/NPC.py
+++ src/sprites/characters/NPC.py
@@ -4,8 +4,8 @@ import pygame
 from src.sprites.Character import *
 
 class NPC(Character):
-    def __init__(self, spriteName):
-        Character.__init__(self, spriteName)
+    def __init__(self, imageFile, spriteSheet):
+        Character.__init__(self, imageFile, spriteSheet)
 
     def move_ai(self, player):
         raise NotImplementedError('Error: Abstract class')
