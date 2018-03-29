4bd686338e4cf809378d66538c1d84da0debcad8
DiegoMartinezCela <diego.martinez.cela@udc.es>
2018-03-29 18:22:14 +0200

    Más bugfixes de la barra de vida

--- src/interface/GUIHealth.py
+++ src/interface/GUIHealth.py
@@ -28,10 +28,10 @@ class GUIHealth(GUIElement):
         # Se coloca el rectangulo en su posicion
         self.set_position(position)
 
-        self.maxLifeCounter = self.guiScreen.player.stats["max_hp"]
-        self.lifeCounter = self.guiScreen.player.stats["hp"]
-
-        for i in range(0, self.lifeCounter):
+        self.maxLifeCounter = 0
+        self.lifeCounter = 0
+        self.gain_max_life()
+        for i in range(0, self.guiScreen.player.stats["hp"]):
             self.gain_life()
 
     def update(self, time):
@@ -48,20 +48,22 @@ class GUIHealth(GUIElement):
         # Implementar animación de perder vida ?
         return
 
-    def gain_life(self):
+    def gain_max_life(self):
         self.maxLifeCounter = self.guiScreen.player.stats["max_hp"]
-        if(self.lifeCount < self.maxLifeCounter):
+
+    def gain_life(self):
+        if(self.lifeCounter < self.maxLifeCounter):
             # Crear corazón y meterlo en array
             heart = GUIImage(self.guiScreen, self.name, self.heartPos, self.scale, self.colorkey)
             self.heartArray.append(heart)
             # Actualizar posiciones del resto de corazones
             self.heartPos = (self.heartPos[0] + heart.image.get_rect().right+2, self.heartPos[1])
-            self.lifeCount+=1
+            self.lifeCounter+=1
 
     def lose_life(self):
         # Eliminar último elemento del array
-        if(len(self.heartArray) > 0 and self.lifeCount > 0):
+        if(len(self.heartArray) > 0 and self.lifeCounter > 0):
             del(self.heartArray[-1])
             # Actualizar posiciones del resto de corazones
             self.heartPos = (self.heartPos[0] - (self.heartArray[-1].image.get_rect().right+2), self.heartPos[1])
-            self.lifeCount-=1
+            self.lifeCounter-=1
