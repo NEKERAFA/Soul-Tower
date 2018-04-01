b950cf334ac3406aef7ad6a9a98386a4381bbf9c
EvaSuarezGarcia <eva.suarez.garci@gmail.com>
2018-04-01 14:12:02 +0200

    Iniciado cambio forzado de personaje

--- src/sprites/PreEndingTrigger.py
+++ src/sprites/PreEndingTrigger.py
@@ -8,9 +8,16 @@ class PreEndingTrigger(ConditionalTrigger):
         if player.choseAnythingNotShared:
             if player.choiceAdder > 0:
                 self.dialogueFile = self.dialogueList[0]
+                if player.currentCharacter != 'sorcerer':
+                    player.change_character()
+                    player.canChange = False
             elif player.choiceAdder < 0:
                 self.dialogueFile = self.dialogueList[1]
+                if player.currentCharacter != 'warrior':
+                    player.change_character()
+                    player.canChange = False
             else:
                 self.dialogueFile = self.dialogueList[randint(0, 1)]
+                
         else:
             self.dialogueFile = self.dialogueList[2]
