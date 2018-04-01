256f8fb3b6ce3db6d8857ef5b802ad658f592e4c
EvaSuarezGarcia <eva.suarez.garci@gmail.com>
2018-04-01 17:54:50 +0200

    Empezados endings

--- src/sprites/Ring.py
+++ src/sprites/Ring.py
@@ -4,9 +4,13 @@ from src.sprites.ConditionalDrop import *
 
 class Ring(ConditionalDrop):
     def collect(self, stage):
-        # TODO modifica la condicion
-        if stage.player.kill:
-            stage.state = OnDialogueState(self.dialogueList[0], stage)
+        # TODO estado para los créditos?
+        # Modifica la condicion
+        if stage.player.choseAnythingNotShared:
+            if stage.player.choiceAdder > 0:
+                stage.state = OnDialogueState(self.dialogueList[0], stage)
+            else:
+                stage.state = OnDialogueState(self.dialogueList[1], stage)
         else:
-            stage.state = OnDialogueState(self.dialogueList[0], stage)
+            stage.state = OnDialogueState(self.dialogueList[2], stage)
         self.kill()
