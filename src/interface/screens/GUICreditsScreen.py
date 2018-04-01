3ad5a8ff7e29093a17ff226fa263813881d51dac
DiegoMartinezCela <diego.martinez.cela@udc.es>
2018-04-01 15:14:05 +0200

    Añadida pantalla de créditos.

--- src/interface/screens/GUICreditsScreen.py
+++ src/interface/screens/GUICreditsScreen.py
@@ -14,28 +14,28 @@ from src.interface.GUIButton import *
 # Pantalla que aparece al terminar el juego
 
 # Localización de los sprites
-#GAME_OVER_TEXT_LOCATION = os.path.join(INTERFACE_GAME_FOLDER, 'gui_game_over_text.png')
+CREDITS_SPRITE_LOCATION = os.path.join(INTERFACE_GAME_FOLDER, 'credits.png')
 
 class GUICreditsScreen(GUIScreen):
     def __init__(self, stage):
         GUIScreen.__init__(self, stage)
         # Texto de muerte
-        self.gameOverText = GUIImage(self, GAME_OVER_TEXT_LOCATION, (0,SCREEN_HEIGHT/2-50))
-
+        self.creditsImage = GUIImage(self, CREDITS_SPRITE_LOCATION, (0,SCREEN_HEIGHT), None, None)
+        
         # Surface para hacer un fadeout
-        self.alpha = 0
+        self.alpha = 255
         self.black = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-        #self.add_element(self.gameOverText)
+        self.add_element(self.creditsImage)
 
     def update(self, time):
         GUIScreen.update(self, time)
-        self.alpha = min(self.alpha + time*0.1, 255)
+        self.alpha = max(self.alpha - time*0.1, 0)
 
     def draw(self, screen):
+        GUIScreen.draw(self, screen)
         self.black.set_alpha(int(self.alpha))
         screen.blit(self.black, (0, 0))
-        GUIScreen.draw(self, screen)
 
     def events(self, event_list):
         return
